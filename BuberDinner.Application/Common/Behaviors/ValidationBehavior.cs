18c20fa65c4459efbad2b7d36d6f228e33bdb383
VN\jason.vu <jason.vu@tpfgroup.cc>
2022-10-17 16:07:10 +0700

    fluentValidator

--- BuberDinner.Application/Common/Behaviors/ValidationBehavior.cs
+++ BuberDinner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -1,13 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using ErrorOr;
+using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
 
 namespace BuberDinner.Application.Common.Behaviors
 {
-    public class ValidationBehavior
+    public class ValidationRegisterCommandBehavior : IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>
     {
+        private readonly IValidator<RegisterCommand> _validator;
 
+        public ValidationRegisterCommandBehavior(IValidator<RegisterCommand> validator)
+        {
+            _validator = validator;
+        }
+
+        public async Task<ErrorOr<AuthenticationResult>> Handle(
+            RegisterCommand request,
+            RequestHandlerDelegate<ErrorOr<AuthenticationResult>> next,
+            CancellationToken cancellationToken)
+        {
+            var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+            if (validationResult.IsValid)
+            {
+                return await next();
+            }
+
+            var errors = validationResult.Errors
+                .ConvertAll(ValidationFailure => Error.Validation(
+                    ValidationFailure.PropertyName, ValidationFailure.ErrorMessage))
+                        .ToList();
+
+            return errors;
+
+        }
     }
 }
--- BuberDinner.Application/DependencyInjection.cs
+++ BuberDinner.Application/DependencyInjection.cs
@@ -1,5 +1,11 @@
-﻿using MediatR;
+﻿using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Common.Behaviors;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace BuberDinner.Application
 {
@@ -8,6 +14,9 @@ namespace BuberDinner.Application
         public static  IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(typeof(DependencyInjection).Assembly);
+            services.AddScoped<IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>, 
+                ValidationRegisterCommandBehavior>();
+            services.AddValidatorsFromAssemblies(Assembly.GetExecutingAssembly());
 
             return services;
         }
